a49b65a2f41bc753c434f7dec9e1d203b0e02c7f
LuisGVicente_UpgradeHub <garcia.vicente@bootcamp-upgrade.com>
2020-10-12 09:29:53 +0200

    Primer borrador

---
+++ index.js
@@ -0,0 +1,35 @@
+function verExperiencia() {
+    var element = document.getElementById('experience');
+    element.classList.add("active");
+    // document.getElementById('studies').classList.remove("active");
+  }
+
+function ocultarExperiencia() {
+    var element = document.getElementById('experience');
+    element.classList.remove("active");
+    // document.getElementById('experience').classList.remove("active");
+  }
+
+function verEstudios() {
+    var element = document.getElementById('studies');
+    element.classList.add("active");
+    // document.getElementById('experience').classList.remove("active");
+  }
+
+function ocultarEstudios() {
+    var element = document.getElementById('studies');
+    element.classList.remove("active");
+    document.getElementById('experience').classList.remove("active");
+  }
+
+function verSkills() {
+    var element = document.getElementById('header-skills');
+    element.classList.add("active");
+    document.getElementById('header-personal').classList.add("inactive");
+}
+
+function ocultarSkills() {
+    var element = document.getElementById('header-personal');
+    element.classList.remove("inactive");
+    document.getElementById('header-skills').classList.remove("active");
+}
